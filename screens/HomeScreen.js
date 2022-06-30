93cbf5d493751cc2378720d6f0a9c21fd4ef55fa
rachanonsomtha <rachanon.som@gmail.com>
2022-06-30 10:18:24 +0700

    Init project

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import HeaderTabs from '../components/HeaderTabs'
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView>
+            <HeaderTabs />
+        </SafeAreaView>
+    )
+}
+         
